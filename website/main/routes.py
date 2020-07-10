16f2bf46e7a0d3aa660df608df92ca45298b1afa
AdamWang00 <AdamWang00@users.noreply.github.com>
2020-07-10 18:47:47 -0400

    pages

--- website/main/routes.py
+++ website/main/routes.py
@@ -7,3 +7,11 @@ main = Blueprint('main', __name__)
 @main.route("/")
 def home():
     return render_template('home.html')
+
+@main.route("/text")
+def text():
+    return render_template('junk_mail.html')
+
+@main.route("/image")
+def image():
+    return render_template('image_rec.html')
