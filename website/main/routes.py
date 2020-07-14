e0fd30ef566d9537b09927b7d9086d8ed7c8c292
AdamWang00 <AdamWang00@users.noreply.github.com>
2020-07-14 17:20:21 -0400

    titles, favicon

--- website/main/routes.py
+++ website/main/routes.py
@@ -6,12 +6,12 @@ main = Blueprint('main', __name__)
 
 @main.route("/")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', title="Home")
 
 @main.route("/text")
 def text():
-    return render_template('junk_mail.html')
+    return render_template('junk_mail.html', title="Junk Mail AI")
 
 @main.route("/image")
 def image():
-    return render_template('image_rec.html')
+    return render_template('image_rec.html', title="Image AI")
