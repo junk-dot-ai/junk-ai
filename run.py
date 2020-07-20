50827028ede9f774e487a61abf955740519c1451
AdamWang00 <AdamWang00@users.noreply.github.com>
2020-07-20 14:39:34 -0400

    debug options

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from website import create_app
+from os import environ
 
 app = create_app() # default config
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    debug = environ.get("DEBUG").lower() == "true" if "DEBUG" in environ else True
+    app.run(debug=debug)
